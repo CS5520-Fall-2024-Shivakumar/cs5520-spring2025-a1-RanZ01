9900c5b84926768fcbfdf21145f608b6ae1f6301
Ran Zhou <zhou.ran1@northeastern.edu>
2025-01-19 23:07:41 -0800

    Android Project Created

--- app/src/main/java/com/example/numad25sp_ranzhou/MainActivity.java
+++ app/src/main/java/com/example/numad25sp_ranzhou/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.numad25sp_ranzhou;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,14 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        TextView txt =findViewById(R.id.textView);
+
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(v -> {
+
+            Toast.makeText(this, "Ran Zhou, zhou.ran1@northeastern.edu", Toast.LENGTH_SHORT).show();
         });
     }
 }
